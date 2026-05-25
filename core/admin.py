0cef1cc82cfeed5d4a263d457c80bcd5047070e8
Janderson Lima dos Santos <estagio.janderson@jfpb.jus.br>
2026-05-25 13:58:10 -0300

    inicia pratica 8

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
