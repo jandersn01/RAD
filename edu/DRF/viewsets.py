ef4926f434ecac7425b9aeee9c701b515d0889f8
janderson01 <jandersonsantos030@gmail.com>
2026-06-07 23:28:38 -0300

    finaliza pratica 09

--- edu/DRF/viewsets.py
+++ edu/DRF/viewsets.py
@@ -1,15 +1,19 @@
 from rest_framework import viewsets
 from edu.models import Autor, Livro, Editora
 from .serializers import AutorSerializer, LivroSerializer, EditoraSerializer
+from rest_framework.permissions import DjangoModelPermissions
 
 class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+    permission_classes = [DjangoModelPermissions]
 
 class AutorViewSet(viewsets.ModelViewSet):
     queryset = Autor.objects.all()
     serializer_class = AutorSerializer
+    permission_classes = [DjangoModelPermissions]
 
 class EditoraViewset(viewsets.ModelViewSet):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+    permission_classes = [DjangoModelPermissions]
